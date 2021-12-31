9339a2594f5159ccb9dbb64a1bdf5cd353ddbdb3
barisdemirkap <barisdemirkap@gmail.com>
2021-12-31 15:23:08 +0300

    initialize of Try and Catch

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System ;
+namespace hata_yonetimi 
+{
+    class Program 
+    {
+        static void Main (string[] args)
+        {
+            try
+            {
+            Console.WriteLine("give me a number") ;
+            int number1 = Convert.ToInt32(Console.ReadLine) ;
+            Console.WriteLine("you enter the number of :" + number1) ;
+
+            
+            }
+            catch(Exception ex)
+            {
+            Console.WriteLine("Hata:" + ex.Message.ToString()) ;
+            }      
+            finally
+            {
+                Console.WriteLine("islem tamamlandi") ;
+            }      
+            
+            Console.WriteLine("give me a number") ;
+
+            try 
+            {
+                int a = int.Parse(null) ;
+                int b = int.Parse("test") ;
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.WriteLine("Bos deger girdiniz") ;
+                Console.WriteLine(ex) ;
+            }
+            try 
+            {
+                int b = int.Parse("test") ;
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Ver' tipi uygun degil") ;
+                Console.WriteLine(ex) ;
+            }
+
+
+        }
+
+
+    }
+}
